6d8c9af1dddf83743527194669e919f114723c09
John Kealy <jdkealy@gmail.com>
2013-10-10 00:46:54 -0400

    new files

--- vendor/assets/javascripts/backbone-forms-rails.js
+++ vendor/assets/javascripts/backbone-forms-rails.js
@@ -1,2 +1,5 @@
 //= require backbone-forms-rails/jquery-ui-editors.js
-//= require backbone-forms-rails/backbone-forms.js
+//= require backbone-forms-rails/backbone-forms.min.js
+//= require backbone-forms-rails/list.js
+//= require backbone-forms-rails/bootstrap.js
+//= require backbone-forms-rails/backbone.bootstrap-modal.min.js
